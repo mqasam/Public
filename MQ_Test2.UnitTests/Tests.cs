b6abd8ce638712d283087ed6d5600e27cce71ca5
Mohammed Qasam <MohammedQasam@ncfe.org.uk>
2022-11-29 11:58:08 +0000

    Update solution

---
+++ MQ_Test2.UnitTests/Tests.cs
@@ -0,0 +1,16 @@
+namespace MQ_Test2.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
